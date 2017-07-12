4a7d6b66fa51a429e96a0dd5f4b6e74c76e85101
JEEVITHAJOTHI <jeevithajothi97@gmail.com>
2017-07-12 10:41:42 +0530

    Add files via upload

---
+++ Greatest.java
@@ -0,0 +1,20 @@
+package Logic;
+import java.util.Scanner;
+public class Greatest {
+
+	public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+int[] a=new int[10];
+int mx=0;
+for(int i=0;i<10;i++)
+{
+	a[i]=s.nextInt();
+	mx=a[0];
+	
+	if(mx<a[i])
+	mx=a[i];
+	}
+System.out.println(mx);
+}
+}
+
