7a0647242bb6087a8d9ef510671fd7e87b1e8bf5
JEEVITHAJOTHI <jeevithajothi97@gmail.com>
2017-07-12 11:47:45 +0530

    Add files via upload

---
+++ ConString.java
@@ -0,0 +1,15 @@
+package Logic;
+
+import java.util.Scanner;
+
+public class ConString {
+
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	String a=s.next();
+	String b=s.next();
+	System.out.println(a+b);
+	
+	}
+
+}
